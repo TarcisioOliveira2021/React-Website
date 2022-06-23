0085e307bb90d85ae64509d11874128def9f6962
TarcisioOliveira2021 <tarcisio.zark.veloso@gmail.com>
2022-06-23 16:24:48 -0300

    Começando e finalizando o sidebar.

---
+++ react-website-smooth-scroll-yt/src/components/Sidebar/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute
+} from './SidebarElements'
+
+const Sidebar = () => {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">About</SidebarLink>
+                    <SidebarLink to="discover">Discover</SidebarLink>
+                    <SidebarLink to="services">Services</SidebarLink>
+                    <SidebarLink to="signup">Sign up</SidebarLink>
+                </SidebarMenu>
+            </SidebarWrapper>
+
+            <SideBtnWrap>
+                <SidebarRoute to="/signin">Sign In</SidebarRoute>
+            </SideBtnWrap>
+
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
